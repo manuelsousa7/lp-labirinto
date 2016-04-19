9ef17481b49616d46a9217747e92faa82c6dfdf5
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2016-04-19 22:56:54 +0100

    distancia added

--- source.pl
+++ source.pl
@@ -1,5 +1,12 @@
-% Gonçalo Marques () && Manuel Sousa (84740)
+% Goncalo Marques () && Manuel Sousa (84740)
 
-%%%%%%%%%%%%%%%%%%%%%% D I S T A N C I A %%%%%%%%%%%%%%%%%%%%%%%%%%
+/******************************************************************************
+* distancia
+*
+* Arguments:   	(L1,C1): Coordenada 1
+*              	(L2,C2): Coordenada 2
+				Dist: Distancia calculada da Coordenada 1 a Coordeanda 2 
+*
+* Description:  Calcula a distancia entre duas coordenadas
+*****************************************************************************/
 distancia((L1,C1),(L2,C2),Dist) :- Dist is abs(L1 - L2) + abs(C1 - C2).
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
