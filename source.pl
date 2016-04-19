d699526e5f8ade80102d2d620262b45b2a6d0314
Manuel Sousa <manuelvsousa@tecnico.ulisboa.pt>
2016-04-19 18:22:12 +0100

    atualizacao + enunciado

--- source.pl
+++ source.pl
@@ -1,2 +1,17 @@
-asdsadsay
-«sdasjkdjasasdasdasdsas
+% Gonçalo Marques () && Manuel Sousa (84740)
+
+%%%%%%%%%%%%%%%%%%%%%% Q U I C K  S O R T %%%%%%%%%%%%%%%%%%%%%%%%%%
+partition([], _, [], []).
+partition([X|Xs], Pivot, Smalls, Bigs) :-
+    (   X @< Pivot ->
+        Smalls = [X|Rest],
+        partition(Xs, Pivot, Rest, Bigs)
+    ;   Bigs = [X|Rest],
+        partition(Xs, Pivot, Smalls, Rest)
+    ).
+
+quicksort([])     --> [].
+quicksort([X|Xs]) -->
+    { partition(Xs, X, Smaller, Bigger) },
+    quicksort(Smaller), [X], quicksort(Bigger).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
