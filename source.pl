b12eaf1d2242e428a7ed3d14c03b1e1fcd0215a2
goncalo <goncalo.cool@gmail.com>
2016-04-21 20:24:23 +0100

    toma la mais uma

--- source.pl
+++ source.pl
@@ -24,11 +24,13 @@ distancia((L1,C1),(L2,C2),Dist) :- Dist is abs(L1 - L2) + abs(C1 - C2).
 * Description:  Calcula a distancia entre duas coordenadas
 *******************************************************************************/
 
-movs_possiveis(Lab, [Pos_X|Pos_Y], [Movs|Ultimo_Mov], Poss) :-	n_esimo(Pos_Y, Lab, Linha),
-																n_esimo(Pos_X, Linha, Restricoes_Lab),
-																cria_lista_restricoes(Res),
-																remove_elementos_iguais(Res, Lab, Restricoes_Finais),
-																cria_poss(Restricoes_Finais, Poss, Movimentos).
+movs_possiveis(Lab, [Pos_X|Pos_Y], [Movs|[Ultima_Dir|Ultima_Pos]], Poss) :-	
+		n_esimo(Pos_Y, Lab, Linha),
+		n_esimo(Pos_X, Linha, Restricoes_Lab),
+		cria_lista_restricoes(Res),
+		remove_elementos_iguais(Res, Lab, Restricoes),
+		remove_elemento(Restricoes, Ultima_Dir, Restricoes_Finais),
+		cria_poss(Restricoes_Finais, [Pos_X|Pos_Y], Poss).
 
 
 /******************************************************************************
@@ -43,33 +45,19 @@ n_esimo(X, [_|L], N) :- N > 1, N1 is N - 1, n_esimo(X, L, N1).
 
 
 % Cria lista de restricoes, para que possam ser eliminadas posteriormente
-cria_lista_restricoes(X) :- X is ["d", "e", "b", "c"].
+cria_lista_restricoes(X) :- X = [d, e, b, c].
 
 
-% Traduz a letra da direcao para uma coordenada
-devolve_coordenada(X, L) :- 	X == "c", L is [-1,0];
-								X == "b", L is [1,0];
-								X == "e", L is [0,-1];
-								X == "d", L is [0,1].
+% Devolve uma lista com a nova posicao apos o movimento
+adiciona_direcao([], [], []).
+adiciona_direcao([H|T], c, L) :- 	H1 is H-1, L = [H1,T].
+adiciona_direcao([H|T], b, L) :- 	H1 is H+1, L = [H1,T].
+adiciona_direcao([H|T], e, L) :- 	T1 is T-1, L = [H,T1].
+adiciona_direcao([H|T], d, L) :- 	T1 is T+1, L = [H,T1].
 
-
-%devolve uma lista com a soma dos elementos de cada indice de duas listas
-adiciona_listas([], [], []).
-adiciona_listas([H1|T1], [H2|T2], [S|L]) :- 	S is H1+H2,
-												adiciona_listas(T1, T2, L).
-
-
-% Cria uma lista de possibilidades de movimento
-cria_poss([], []).
-cria_poss([H1|T1], Poss_Lista, [[H1|Posicao] |L]) :- 	devolve_coordenada(H1, Movimento),
-														adiciona_listas(Poss_Lista, Movimento, Posicao),
-														cria_poss(T1, L).
-
-
-
-% Remove os elementos iguais a X da lista [H|T1]
+% Remove os elementos iguais a X da lista
 remove_elemento([], _, []).
-remove_elemento([H|T1], H, L) :- 	remove_elemento(T1, H, L).
+remove_elemento([H|T1], H, L) :- 		remove_elemento(T1, H, L).
 remove_elemento([H|T1], X, [H|L]) :-	H =\= X, remove_elemento(T1, X, L).
 
 
@@ -79,12 +67,19 @@ remove_elementos_iguais(H1, [H2|T2], L) :- 		remove_elemento(H1, H2, L1),
 												remove_elementos_iguais(L1, T2, L).
 
 
+% Cria uma lista de possibilidades de movimento
+cria_poss([], []).
+cria_poss([H|T], Poss_Lista, [Lista|L]) :-		adiciona_direcao(Poss_Lista, H, Posicao),
+												append(H, Posicao, Lista),
+												cria_poss(T, L).
+
+
 /******************************************************************************
 * ordena
 *
 *******************************************************************************/
 append([], X, X).
-append([X|L1], L2, [X|L3]) :- 		append(L1, L2, L3).
+append([X|L1], L2, [X|L3]) :- 	append(L1, L2, L3).
 
 partition(_, [], [], []).
 partition(A, [H|T], [H|P], S) :- 	A >= H,
