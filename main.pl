975181ad755273f708eae769d209a4819c3f1e73
Gonçalao <Gonalo Marques>
2016-05-05 13:34:14 +0100

    toma la

--- main.pl
+++ main.pl
@@ -6,7 +6,7 @@
 * Arguments:   	Lab 			Labirinto
 *				(Pos_X, Pos_Y) 	Posicao atual
 *				Movimentos 		Lista com os movimentos efetuados
-*				Poss 			Movimentos possiveis - RESULTADO
+*				Poss 			Movimentos possiveis - PRETENDIDO
 *
 * Description:  Devolve uma lista com os movimentos possiveis
 *******************************************************************************/
@@ -16,21 +16,19 @@ movs_possiveis(Lab, (Pos_X, Pos_Y), Movimentos, Poss_Final) :-
 		n_esimo(Pos_Y, Linha, Restricoes_Lab),
 		remove_elementos_iguais([c, b, e, d], Restricoes_Lab, Restricoes),
 		cria_poss(Restricoes, (Pos_X, Pos_Y), Movimentos, Poss),
-		remove_elemento(Poss, [], Poss_Final).
-
+		remove_elemento(Poss, [], Poss_Final), !.
 
 /******************************************************************************
 * distancia /3
 *
 * Arguments:   	(L1,C1)			Coordenada 1
 *				(L2,C2)			Coordenada 2
-*				Dist 			Distancia entre as duas coordenadas	- RESULTADO
+*				Dist 			Distancia entre as duas coordenadas	- PRETENDIDO
 *
 * Description:  Calcula a distancia entre duas coordenadas
 *******************************************************************************/
 
 distancia((L1,C1),(L2,C2),Dist) :- Dist is abs(L1 - L2) + abs(C1 - C2).
-
 
 /******************************************************************************
 * resolve1 /4
@@ -39,14 +37,14 @@ distancia((L1,C1),(L2,C2),Dist) :- Dist is abs(L1 - L2) + abs(C1 - C2).
 *				Pos_Inicial		Posicao inicial
 *				Pos_Final 		Posicao final
 *				Pos_Atual		Posicao atual
-*				Lista_Movs		Lista com os movimentos efetuados - RESULTADO
+*				Lista_Movs		Lista com os movimentos efetuados - PRETENDIDO
 *
 * Description:  Resolve um labirinto na ordem c,b,e,d
 *******************************************************************************/
 
 resolve1(Lab, Pos_Inicial, Pos_Final, Lista_Movs) :-
 					movs_possiveis(Lab, Pos_Inicial, [(i, Pos_Inicial)], Poss),
-					resolve1(Lab,  Pos_Final, Pos_Inicial, Poss, [(i, Pos_Inicial)], Lista_Movs).
+					resolve1(Lab, Pos_Final, Pos_Inicial, Poss, [(i, Pos_Inicial)], Lista_Movs), !.
 					
 resolve1(_, Pos_Final, Pos_Final, _, Lista_Movs, Lista_Movs) :- !.
 
@@ -70,7 +68,7 @@ resolve1(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], Movs, Lista_Movs) 
 *				Pos_Inicial		Posicao inicial
 *				Pos_Final 		Posicao final
 *				Pos_Atual		Posicao atual
-*				Lista_Movs		Lista com os movimentos efetuados - RESULTADO 	
+*				Lista_Movs		Lista com os movimentos efetuados - PRETENDIDO 	
 *
 * Description:  Resolve um labirinto tendo em conta qual das solucoes esta mais proxima do final
 *******************************************************************************/
@@ -84,25 +82,23 @@ resolve2(_, Pos_Final, Pos_Final, _, Lista_Movs, Lista_Movs) :- !.
 
 resolve2(_, _, _, [], _, _).
 
-resolve2(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], [(Dir_Ini, X_Ini, Y_Ini), Movs_Resto], Lista_Movs) :-
+resolve2(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], [(Dir_Ini, X_Ini, Y_Ini)|Movs_Resto], Lista_Movs) :-
 					var(Lista_Movs),(
 						Pos_Final \= Pos_Atual,
 						[(Dir, X, Y)|Poss_Resto] \= [],
-						append([(Dir_Ini, X_Ini, Y_Ini), Movs_Resto], [(Dir, X, Y)], Movimentos),
+						append([(Dir_Ini, X_Ini, Y_Ini)|Movs_Resto], [(Dir, X, Y)], Movimentos),
 						movs_possiveis(Lab, (X,Y), Movimentos, Possibilidades),
 						ordena_poss(Possibilidades, Poss, (X_Ini, Y_Ini), Pos_Final),
 						resolve2(Lab,  Pos_Final, (X,Y), Poss, Movimentos, Lista_Movs),
-						resolve2(Lab, Pos_Final, Pos_Atual, Poss_Resto, [(Dir_Ini, X_Ini, Y_Ini), Movs_Resto], Lista_Movs)
+						resolve2(Lab, Pos_Final, Pos_Atual, Poss_Resto, [(Dir_Ini, X_Ini, Y_Ini)|Movs_Resto], Lista_Movs)
 					);
 					!.
-
 
 /******************************************************************************
 *
 * predicados auxiliares (usados no movs_possiveis)
 *
 *******************************************************************************/
-
 
 % Devolve o N-esimo elemento de uma lista
 n_esimo(1, [X|_], X).
@@ -144,19 +140,17 @@ cria_poss([H|T], Poss_Lista, Movs, [Lista|L]) :-
 					verifica_percorrida((H, Posicao), Movs, Lista),
 					cria_poss(T, Poss_Lista, Movs, L).
 
-
-
-
 /******************************************************************************
 * ordena_poss /4
 *
 * Arguments:   	Possibilidades de movimento
-*				Possibilidades de movimento organizadas - RESULTADO
+*				Possibilidades de movimento organizadas - PRETENDIDO
 *				Posicao inicial
 *				Posicao final  	
 *
 * Description:  Ordena a lista de movimentos possiveis (baseado em InsertionSort)
 *******************************************************************************/
+
 ordena_poss([],[],(_,_),(_,_)).
 ordena_poss([(A,X,Y)|Xs],Ys,(Ix,Iy),(Fx,Fy)) :-
 						ordena_poss(Xs,Zs,(Ix,Iy),(Fx,Fy)), 
