6f4e7e16527caece0b3069663161850a22c338a1
goncalo <goncalo.cool@gmail.com>
2016-05-08 22:41:19 +0100

    final

--- lp-g1.pl
+++ lp-g1.pl
@@ -3,10 +3,10 @@
 /******************************************************************************
 * movs_possiveis /4
 *
-* Arguments:   	Lab 			Labirinto
-*				(Pos_X, Pos_Y) 	Posicao atual
-*				Movimentos 		Lista com os movimentos efetuados
-*				Poss 			Movimentos possiveis - PRETENDIDO
+* Arguments:    Lab             Labirinto
+*               (Pos_X, Pos_Y)  Posicao atual
+*               Movimentos      Lista com os movimentos efetuados
+*               Poss            Movimentos possiveis - PRETENDIDO
 *
 * Description:  Devolve uma lista com os movimentos possiveis
 *******************************************************************************/
@@ -31,22 +31,22 @@ n_esimo(N, [_|L], X) :- N > 1, N1 is N - 1, n_esimo(N1, L, X).
 
 % Devolve um tuplo com a nova posicao apos o movimento
 adiciona_direcao([], [], []).
-adiciona_direcao((H,T), c, L) :- 	H1 is H-1, L = (H1,T).
-adiciona_direcao((H,T), b, L) :- 	H1 is H+1, L = (H1,T).
-adiciona_direcao((H,T), e, L) :- 	T1 is T-1, L = (H,T1).
-adiciona_direcao((H,T), d, L) :- 	T1 is T+1, L = (H,T1).
+adiciona_direcao((H,T), c, L) :-    H1 is H-1, L = (H1,T).
+adiciona_direcao((H,T), b, L) :-    H1 is H+1, L = (H1,T).
+adiciona_direcao((H,T), e, L) :-    T1 is T-1, L = (H,T1).
+adiciona_direcao((H,T), d, L) :-    T1 is T+1, L = (H,T1).
 
 
 % Remove os elementos iguais a X da lista
 remove_elemento(H, x, H) :- !.
 remove_elemento([], _, []) :- !.
-remove_elemento([H|T1], H, L) :- 		remove_elemento(T1, H, L).
-remove_elemento([H|T1], X, [H|L]) :-	H \= X, remove_elemento(T1, X, L).
+remove_elemento([H|T1], H, L) :-        remove_elemento(T1, H, L).
+remove_elemento([H|T1], X, [H|L]) :-    H \= X, remove_elemento(T1, X, L).
 
 
 % Remove da primeira lista elementos que sejam iguais aos da segunda
 remove_elementos_iguais(L, [], L) :- !.
-remove_elementos_iguais(H1, [H2|T2], L) :- 		
+remove_elementos_iguais(H1, [H2|T2], L) :-      
 					remove_elemento(H1, H2, L1),
 					remove_elementos_iguais(L1, T2, L).
 
@@ -68,9 +68,9 @@ verifica_percorrida((Dir, Pos_X, Pos_Y), [(_,Movs_X, Movs_Y)|Cauda], Lista) :-
 /******************************************************************************
 * distancia /3
 *
-* Arguments:   	(L1,C1)			Coordenada 1
-*				(L2,C2)			Coordenada 2
-*				Dist 			Distancia entre as duas coordenadas	- PRETENDIDO
+* Arguments:    (L1,C1)         Coordenada 1
+*               (L2,C2)         Coordenada 2
+*               Dist            Distancia entre as duas coordenadas - PRETENDIDO
 *
 * Description:  Calcula a distancia entre duas coordenadas
 *******************************************************************************/
@@ -80,11 +80,11 @@ distancia((L1,C1),(L2,C2),Dist) :- Dist is abs(L1 - L2) + abs(C1 - C2).
 /******************************************************************************
 * resolve1 /4
 *
-* Arguments:   	Lab 			Labirinto
-*				Pos_Inicial		Posicao inicial
-*				Pos_Final 		Posicao final
-*				Pos_Atual		Posicao atual
-*				Lista_Movs		Lista com os movimentos efetuados - PRETENDIDO
+* Arguments:    Lab             Labirinto
+*               Pos_Inicial     Posicao inicial
+*               Pos_Final       Posicao final
+*               Pos_Atual       Posicao atual
+*               Lista_Movs      Lista com os movimentos efetuados - PRETENDIDO
 *
 * Description:  Resolve um labirinto na ordem c,b,e,d
 *******************************************************************************/
@@ -111,11 +111,11 @@ resolve1(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], Movs, Lista_Movs) 
 /******************************************************************************
 * resolve2 /4
 *
-* Arguments:   	Lab 			Labirinto
-*				Pos_Inicial		Posicao inicial
-*				Pos_Final 		Posicao final
-*				Pos_Atual		Posicao atual
-*				Lista_Movs		Lista com os movimentos efetuados - PRETENDIDO 	
+* Arguments:    Lab             Labirinto
+*               Pos_Inicial     Posicao inicial
+*               Pos_Final       Posicao final
+*               Pos_Atual       Posicao atual
+*               Lista_Movs      Lista com os movimentos efetuados - PRETENDIDO  
 *
 * Description:  Resolve um labirinto tendo em conta qual das solucoes esta mais proxima do final
 *******************************************************************************/
@@ -144,10 +144,10 @@ resolve2(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], [(Dir_Ini, X_Ini, 
 /******************************************************************************
 * ordena_poss /4
 *
-* Arguments:   	Possibilidades de movimento
-*				Possibilidades de movimento organizadas - PRETENDIDO
-*				Posicao inicial
-*				Posicao final  	
+* Arguments:    Possibilidades de movimento
+*               Possibilidades de movimento organizadas - PRETENDIDO
+*               Posicao inicial
+*               Posicao final   
 *
 * Description:  Ordena a lista de movimentos possiveis (baseado em InsertionSort)
 *******************************************************************************/
@@ -155,23 +155,23 @@ resolve2(Lab, Pos_Final, Pos_Atual, [(Dir, X, Y)|Poss_Resto], [(Dir_Ini, X_Ini, 
 ordena_poss([],[],(_,_),(_,_)).
 ordena_poss([(A,Pos_X1,Pos_Y1)|Xs],Cauda,(IX,IY),(FX,FY)) :-
 					ordena_poss(Xs,Zs,(IX,IY),(FX,FY)), 
-					!, 
+					!,
 					ordena((A,Pos_X1,Pos_Y1),Zs,Cauda,(IX,IY),(FX,FY)).
 ordena((A,Pos_X1,Pos_Y1),[],[(A,Pos_X1,Pos_Y1)],(_,_),(_,_)).
 
-ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(B,Pos_X2,Pos_Y2)|Zs],(IX,IY),(FX,FY)) :- 	
+ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(B,Pos_X2,Pos_Y2)|Zs],(IX,IY),(FX,FY)) :-   
 					distancia((Pos_X1,Pos_Y1),(FX,FY),Dist1),
 					distancia((Pos_X2,Pos_Y2),(FX,FY),Dist2),
 					Dist1 > Dist2, 
 					!, 
 					ordena((A,Pos_X1,Pos_Y1),Cauda,Zs,(IX,IY),(FX,FY)).
 
-ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(_,_),(FX,FY)) :- 	
+ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(_,_),(FX,FY)) :-    
 					distancia((Pos_X1,Pos_Y1),(FX,FY),Dist1),
 					distancia((Pos_X2,Pos_Y2),(FX,FY),Dist2),
 					Dist1 < Dist2.
 
-ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(B,Pos_X2,Pos_Y2)|Zs],(IX,IY),(FX,FY)) :- 	
+ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(B,Pos_X2,Pos_Y2)|Zs],(IX,IY),(FX,FY)) :-   
 					distancia((Pos_X1,Pos_Y1),(FX,FY),Dist1),
 					distancia((Pos_X2,Pos_Y2),(FX,FY),Dist2),
 					Dist1 == Dist2,
@@ -182,7 +182,7 @@ ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(B,Pos_X2,Pos_Y2)|Zs],(IX,IY
 					ordena((A,Pos_X1,Pos_Y1),Cauda,Zs,(IX,IY),(FX,FY)).
 
 
-ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(IX,IY),(FX,FY)) :- 	
+ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(IX,IY),(FX,FY)) :-  
 					distancia((Pos_X1,Pos_Y1),(FX,FY),Dist1),
 					distancia((Pos_X2,Pos_Y2),(FX,FY),Dist2),
 					Dist1 == Dist2,
@@ -190,7 +190,7 @@ ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,
 					distancia((Pos_X2,Pos_Y2),(IX,IY),Dist22),
 					Dist11 > Dist22.
 
-ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(IX,IY),(FX,FY)) :- 	
+ordena((A,Pos_X1,Pos_Y1),[(B,Pos_X2,Pos_Y2)|Cauda],[(A,Pos_X1,Pos_Y1),(B,Pos_X2,Pos_Y2)|Cauda],(IX,IY),(FX,FY)) :-  
 					distancia((Pos_X1,Pos_Y1),(FX,FY),Dist1),
 					distancia((Pos_X2,Pos_Y2),(FX,FY),Dist2),
 					Dist1 == Dist2,
